b00beccf870f641841441d2db0b60e1bfc575c80
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-02-21 11:39:00 +0000

    Update variables.tf

--- Demo/variables.tf
+++ Demo/variables.tf
@@ -1,9 +1,9 @@
-#Input Variables
-# variable "aws_region" {
-#   description = "Region in which AWS resource to be created"
-#   type = string
-#   default = "us-east-1"
-# }
+Input Variables
+variable "aws_region" {
+  description = "Region in which AWS resource to be created"
+  type = string
+  default = "us-east-1"
+}
 
 variable "cidr_addr_range" {
     type = string  
@@ -14,32 +14,3 @@ variable "input_vpc_name" {
   description = "Set the name of the VPC"
 }
 
-# variable "ec2_ami_id" {
-#   description = "AMI ID"
-#   type = string
-#   default = "ami-......" # Amazon linux 2 AMI ID
-# }
-
-# variable "ec2_instance_count" {
-#     type = number
-#     default = 1
-# }
-
-
-# variable "vpcname" {
-#   type = string
-#   default = "myvpc"
-# }
-
-# variable "sshport" {
-#   type = number
-#   default = 22
-# }
-
-# # variable = "enabled" {
-# #     default = true
-# # }
-# variable "mylist" {
-#   type = list(string)
-#   default = ["value1", "value2"]
-# }
