f61ec2397f1e616d5d62c22087202a48d443b416
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-02-21 13:17:41 +0000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+Input Variables
+variable "aws_region" {
+  description = "Region in which AWS resource to be created"
+  type = string
+  default = "us-east-1"
+}
+
+variable "cidr_addr_range" {
+    type = string  
+}
+
+variable "input_vpc_name" {
+  type = string
+  description = "Set the name of the VPC"
+}
