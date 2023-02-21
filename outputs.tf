a9726645f755086ce2cb39a02623e69a32c27491
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-02-21 13:21:35 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,16 +6,3 @@ output "myoutput" {
 output "my_vpc_name_output" {
   value = aws_vpc.vpc-dev.tags
 }
-
-variable "mytuple" {
-  type = tuple(["string", number, "string"])
-  default = ["hello" , 1  , "world"]
-}
-
-variable "myobject" {
-  type = object({name = string, port = list(number)})
-  default = {
-    name = "gl"
-    port = [ 22, 25, 80 ]
-  }
-}
