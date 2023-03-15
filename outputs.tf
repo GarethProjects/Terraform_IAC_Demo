2f2800cd75b22e905a8aaf9749274acbed10437b
GarethProjects <64733108+GarethProjects@users.noreply.github.com>
2023-03-15 17:51:58 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 # Outputs
 output "myoutput" {
-  value = aws_vpc.vpc-dev.id
+  value = aws_vpc.vpc-dev-demopipeline.id
 }
 
 output "my_vpc_name_output" {
-  value = aws_vpc.vpc-dev.tags
+  value = aws_vpc.vpc-dev-demopipeline.tags
 }
